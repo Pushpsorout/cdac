34783c2f759049b99291d2652892b6bed018fa52
Pushpsorout <53961784+Pushpsorout@users.noreply.github.com>
2022-03-25 17:31:19 +0530

    Delete quadeatic.py

--- quadeatic.py
+++
@@ -1,9 +0,0 @@
-import cmath
-
-a=int(input("enter your first value"))
-b=int(input("enter your second value"))
-c=int(input("enter your third value"))
-
-sol=(-b-cmath.sqrt(b**2-4*a*c))/2*a
-sol2=(-b+cmath.sqrt(b**2-4*a*c))/2*a
-print(sol,"\n",sol2)
