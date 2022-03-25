3a0563c85045cc41b21464253da3972b364ccaa1
Pushpsorout <53961784+Pushpsorout@users.noreply.github.com>
2022-03-25 17:31:32 +0530

    Delete str.py

--- str.py
+++
@@ -1,7 +0,0 @@
-a="""Twinkle,twinkle,little star,
-             How I wonder what you are!
-                     Up above the world so high,
-                     Like a dimond in the sky.
-      Twinkle, twinkle, little star,
-                How I wonder what you are"""
-print(a)
